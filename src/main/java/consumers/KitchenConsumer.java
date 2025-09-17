faaae30cc5aa8c69e59d350666120eec879761d5
Murilo Lucena <muriloglucenacanal@gmail.com>
2025-09-17 08:50:23 -0300

    feat: adição do consumidor de cozinha

---
+++ src/main/java/consumers/KitchenConsumer.java
@@ -0,0 +1,33 @@
+package consumers;
+
+import com.rabbitmq.client.*;
+import java.nio.charset.StandardCharsets;
+
+public class KitchenConsumer {
+    private static final String EXCHANGE = "orders"; //Aqui definimos de QUAL exchange estamos recebendo
+    private static final String QUEUE = "kitchen_queue";
+    private static final String ROUTING_KEY = "order.kitchen";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE, BuiltinExchangeType.DIRECT, true);
+
+        //Respectivamente: Nome da fila, a "durabilidade" da fila, se a fila some automaticamente se não tem mais consumidores, se tem algum parâmetro personalidado:
+        channel.queueDeclare(QUEUE, true, false, false, null);
+        channel.queueBind(QUEUE, EXCHANGE, ROUTING_KEY);
+
+        System.out.println("[Cozinha] - Aguardando pedidos...");
+
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            String msg = new String(delivery.getBody(), StandardCharsets.UTF_8); //Aqui vai extrair o corpo da requisição
+            System.out.println("Pedido recebido: " + msg);
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        };
+
+        channel.basicConsume(QUEUE, false, callback, consumerTag -> {});
+    }
+}
